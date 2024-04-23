e6dd13175ea0af0e05b8faa579e09c0a5be6386c
AbdelrahmanAly6 <149795197+AbdelrahmanAly6@users.noreply.github.com>
2024-04-24 01:06:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,12 @@ btn.addEventListener("click", function () {
   } else if (user_guess > random) {
     counter++;
     result.style.color = "red";
-    result.innerHTML = " You Guessed It Wrong, Try Lower Number #" + counter - 1;
+    result.innerHTML =
+      " You Guessed It Wrong, Try Lower Number #" + (counter - 1);
   } else {
     counter++;
     result.style.color = "red";
-    result.innerHTML = " You Guessed It Wrong, Try Higher Number #" + counter -1;
+    result.innerHTML =
+      " You Guessed It Wrong, Try Higher Number #" + (counter - 1);
   }
 });
