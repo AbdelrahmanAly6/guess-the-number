e5eaede856cda6d3e89d59c23a6e8b21ee6314d7
AbdelrahmanAly6 <149795197+AbdelrahmanAly6@users.noreply.github.com>
2024-04-24 00:44:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let random = Math.floor(Math.random() * 100);
+
+let btn = document.getElementById("btn");
+
+let result = document.getElementById("result");
+
+let counter = 1;
+
+btn.addEventListener("click", function () {
+  let user_guess = document.getElementById("user_guess").value;
+
+  if (user_guess == random) {
+    result.style.color = "green";
+    result.innerHTML = " You Guessed It Right In " + counter + " Guesses !";
+  } else if (user_guess > random) {
+    counter++;
+    result.style.color = "red";
+    result.innerHTML = " You Guessed It Wrong, Try Lower Number";
+  } else {
+    counter++;
+    result.style.color = "red";
+    result.innerHTML = " You Guessed It Wrong, Try Higher Number";
+  }
+});
